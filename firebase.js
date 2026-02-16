d5876af28271aa33a7b4d01c9ae4f538a46daeb3
yogesh sharma <yogeshsharmma2003kh@gmail.com>
2026-02-16 15:43:23 +0530

    done

--- firebase.js
+++ firebase.js
@@ -9,6 +9,9 @@
 
 
 import admin from "firebase-admin";
+// import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
+
+
 
 
 admin.initializeApp({
@@ -19,5 +22,8 @@ admin.initializeApp({
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   }),
 });
+console.log("PROJECT ID:", process.env.FIREBASE_PROJECT_ID);
+console.log("CLIENT EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
+console.log("PRIVATE KEY EXISTS:", !!process.env.FIREBASE_PRIVATE_KEY);
 
 export default admin;

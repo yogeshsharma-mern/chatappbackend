92e59acefb3ca58fbb5234715b489bebc14d1bf4
yogesh sharma <yogeshsharmma2003kh@gmail.com>
2026-02-16 15:25:09 +0530

    done

--- firebase.js
+++ firebase.js
@@ -9,10 +9,15 @@
 
 
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
+
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  // credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
 });
 
 export default admin;
